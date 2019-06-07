6885721d00d9eb931ea51eec3530dec48db58820
Laura Aubin <laura.aubin@shopify.com>
2019-06-06 23:56:53 -0400

    Read file contents from the client react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello from the client!
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { readFile: 'empty' };
+  }
+
+  componentDidMount() {
+    this.readFile('./server.js');
+  }
+
+  render() {
+    const { readFile } = this.state;
+
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          File contents of server.js:
+          <div className='App-fileOutput'>{readFile}</div>
+        </header>
+      </div>
+    );
+  }
+
+  fetchRequest = async (type, path, body) => {
+    return await fetch(path, {
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: type.toUpperCase()
+    });
+  };
+
+  readFile = async file => {
+    await this.fetchRequest('post', 'readFile', { file })
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ readFile: data.fileContents });
+      });
+  };
 }
 
 export default App;
