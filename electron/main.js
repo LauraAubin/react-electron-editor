37d9d8c8ba4bacd726366a7cae17cd0c79d8c7ec
Laura Aubin <laura.aubin@shopify.com>
2019-06-07 18:29:45 -0400

    Create an Electron application

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function() {
+  if (mainWindow === null) createWindow();
+});
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+  });
+
+  mainWindow.loadURL('http://localhost:3000/');
+
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
