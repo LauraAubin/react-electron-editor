489352bab378ee23bf73a64ca5d255208d597ecc
Laura Aubin <laura.aubin@shopify.com>
2019-08-20 12:19:55 -0400

    Check environment in Electron and access the compiled React app

--- client/public/electron.js
+++ client/public/electron.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 
+const path = require('path');
+const isDev = require('electron-is-dev');
+
 let mainWindow;
 
 app.on('ready', createWindow);
@@ -15,10 +18,14 @@ app.on('activate', function() {
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1400,
-    height: 900,
+    height: 900
   });
 
-  mainWindow.loadURL('http://localhost:3000/');
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000/'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
 
   // mainWindow.webContents.openDevTools();
 
